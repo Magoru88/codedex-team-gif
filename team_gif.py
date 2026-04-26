f3a6d8fe2603604f3531bc8e51cb943dad0d7885
Magoru88 <rubengonzalezmadrid88@gmail.com>
2026-04-26 18:07:37 +0200

    Create team_gif.py

---
+++ team_gif.py
@@ -0,0 +1,9 @@
+import imageio.v3 as iio
+
+filesnames = ['team-pic1.png', 'team-pic2.png']
+images = []
+
+for filename in filesnames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('team.gif', images, duration = 500, loop = 0)
